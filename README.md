607f3606a52354431dc36976a011071082d171e2
richardgmartin <richardgeorgemartin@gmail.com>
2016-07-19 16:21:28 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+photoLogger is an iOS app, developed in Swift, for mobile professionals who want to capture topic information including a photo, description, title, date, and address.
+
---
+++ photoLogger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photoLogger
+//
+//  Created by Richard Martin on 2016-07-19.
+//  Copyright © 2016 richard martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
