6b6fa96784479f2bdc682f334fddd7069b187345
richardgmartin <richardgeorgemartin@gmail.com>
2016-07-21 18:34:22 -0400

    added Navigation Bar, Bar Button (to Add post), TableView, Prototype Cell, contrained margins, and delegate code for TableView

--- photoLogger/DetailViewController.swift
+++ photoLogger/DetailViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCellID")
+        return cell!
+    }
+    
+    internal func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
 
 
 
