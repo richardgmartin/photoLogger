affd9830e7095b5f9ea98dc77272a4ef86062484
richardgmartin <richardgeorgemartin@gmail.com>
2016-08-17 15:53:27 -0400

    enable email auth in forebase; hooked up text field outlets and signin action; coded firebase auth logic for email login and user creation

--- photoLogger/AppDelegate.swift
+++ photoLogger/AppDelegate.swift
@@ -18,22 +18,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FIRApp.configure()
     }
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
-        
-        
     }
     
     func application(_ app: UIApplication, open url: URL, sourceApplication: String?, annotation: AnyObject) -> Bool {
-        
         return FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: sourceApplication, annotation: annotation)
-        
     }
-    
-    
     
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- photoLogger/LoginView.swift
+++ photoLogger/LoginView.swift
@@ -11,6 +11,10 @@ import Firebase
 
 class LoginView: UIViewController {
 
+    @IBOutlet weak var emailAddressText: TextFieldView!
+    @IBOutlet weak var passwordText: TextFieldView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -46,11 +50,38 @@ class LoginView: UIViewController {
         
         FIRAuth.auth()?.signIn(with: _credential, completion: { (user, error) in
             if error != nil {
+                // firebase authentication failed
                 print("RGM: unable to authenticate with Firebase - \(error)")
             } else {
+                // firebase authentication successful
                 print("RGM: successfully authenticated with Firebase")
             }
         })
     }
 
+    @IBAction func signinTapped(_ sender: AnyObject) {
+        if let email = emailAddressText.text, let password = passwordText.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error == nil {
+                    // firebase authentication successful
+                    print("RGM: Email user successfully authenticated with Firebase")
+                } else {
+                    // user does not exist in firebase :: create new user
+                    FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if error != nil {
+                            // problem creating user
+                            print("RGM: problem creating new user from email and password")
+                        } else {
+                            // new user successfully created in firebase
+                            print("RGM: new user from email and password successfully created")
+                        }
+                    })
+                }
+            })
+        } else {
+            // send alert to user (when we go to production
+            print("RGM: email address and/or password fields empty")
+        }
+        
+    }
 }
